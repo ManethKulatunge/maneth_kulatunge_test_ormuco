407f9b65807a3ca27ce21067f3a6d8f4e167c80f
ManethKulatunge <43322057+ManethKulatunge@users.noreply.github.com>
2020-02-21 19:28:07 -0500

    Question A added

--- QuestionA.java
+++ QuestionA.java
@@ -3,11 +3,14 @@ package maneth_kulatunge_testOrmuco;
 public class QuestionA {
 	public static Boolean Overlap(int[][] lines) {
 	
+		//checking if first line is completely bigger than the second
 		Boolean isLineBigger = (lines[0][0] >= lines[1][0] &&
 				 lines[0][1] >= lines[1][1] 
 				);
-		
+		//checking if there is an overlap in the starting points
 		Boolean overlapOfStarting = (lines[0][0] < lines[0][1] && lines[0][1] > lines [1][0]);
+		
+		//checking if there is an overlap in final points
 		Boolean overlapOfEnding = (lines[0][1] < lines[1][1] && lines[0][1] > lines [1][0]);
 		
 		Boolean result = isLineBigger || overlapOfStarting || overlapOfEnding ;
@@ -19,7 +22,6 @@ public class QuestionA {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//example
-		//int [][] lines = new int[][] { {1,3}, {2,4} };
-		
+		//int [][] lines = new int[][] { {1,3}, {1,4} };
 	}
 }
